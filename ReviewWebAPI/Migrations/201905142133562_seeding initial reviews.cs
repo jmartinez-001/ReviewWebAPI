c8cbe16d2adcc5b6b0ec4c0a0147b8d5b6401395
Jose Martinez <jose.a.m089@gmail.com>
2019-05-14 16:41:01 -0500

    Seeded Database with initial reviews to begin testing GET method.

---
+++ ReviewWebAPI/Migrations/201905142133562_seeding initial reviews.cs
@@ -0,0 +1,16 @@
+namespace ReviewWebAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedinginitialreviews : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
